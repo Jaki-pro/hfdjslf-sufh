591302ee7196394a5be84f10193c4bc31ff315ff
Ahmed jaki <70642638+Jaki-pro@users.noreply.github.com>
2021-01-02 22:21:34 +0600

    Add files via upload
    rtewtgeg

---
+++ 28.js
@@ -0,0 +1,32 @@
+
+
+
+
+document.querySelector("#myBtn").addEventListener("click",function(){
+	
+	if(document.querySelector(".show")){
+   document.querySelector("div").classList.remove("show");
+	}
+	
+	else{
+		document.querySelector("div").classList.add("show");
+	}
+		
+		
+	
+	//document.querySelector("div").classList.toggle("show");
+	
+});
+
+
+
+
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
